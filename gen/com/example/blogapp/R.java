67e9f99989932f2213f0b9046890b406e82b5c28
mike <mikelewek@gmail.com>
2015-09-28 00:40:34 -0400

    first commit

--- gen/com/example/blogapp/R.java
+++ gen/com/example/blogapp/R.java
@@ -36,7 +36,6 @@ public final class R {
         public static final int drawer_shadow=0x7f020000;
         public static final int ic_drawer=0x7f020001;
         public static final int ic_launcher=0x7f020002;
-        public static final int screen1=0x7f020003;
     }
     public static final class id {
         public static final int action_close=0x7f090006;
